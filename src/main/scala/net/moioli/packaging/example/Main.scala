63cbc11a620a6f7c38eebc5807a83ae899bbf733
Silvio Moioli <silvio@moioli.net>
2013-02-16 17:45:33 +0100

    Added Selenium with a working example

--- src/main/scala/net/moioli/packaging/example/Main.scala
+++ src/main/scala/net/moioli/packaging/example/Main.scala
@@ -1,10 +1,22 @@
 package net.moioli.packaging.example
 
+import java.util.concurrent.TimeUnit
+
+import org.openqa.selenium.By
+import org.openqa.selenium.htmlunit.HtmlUnitDriver
+import org.hamcrest.CoreMatchers
+
 object Main {
 
   def main(args: Array[String]): Unit = {
-    val ciao = "ciao"
-    System.out.println(s"Hia! $ciao")
+    val driver = new HtmlUnitDriver()
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS)
+    driver.get("http://docs.seleniumhq.org/")
+    driver.findElement(By.linkText("Projects")).click()
+    driver.findElement(By.linkText("Download")).click()
+    driver.findElement(By.linkText("Source Code")).click()
+    driver.findElement(By.xpath("//a[contains(text(),'Browse\n        SVN')]")).click()
+    System.out.println(driver.getCurrentUrl())
   }
 
 }
